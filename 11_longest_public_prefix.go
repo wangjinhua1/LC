53de8f7622f7be63a141f5f1f4f885dd25e87733
jinhua.wang <jinhua.wang@transwarp.io>
2022-02-27 12:21:56 +0800

    20220227:11_longest_public_prefix

---
+++ 11_longest_public_prefix.go
@@ -0,0 +1,64 @@
+// 14. 最长公共前缀
+// 编写一个函数来查找字符串数组中的最长公共前缀。
+// 如果不存在公共前缀，返回空字符串 ""。
+//
+// 示例 1：
+// 输入：strs = ["flower","flow","flight"]
+// 输出："fl"
+// 示例 2：
+// 输入：strs = ["dog","racecar","car"]
+// 输出：""
+// 解释：输入不存在公共前缀。
+
+// https://leetcode-cn.com/problems/longest-common-prefix/solution/zui-chang-gong-gong-qian-zhui-by-leetcode-solution/
+// 方法一：横向扫描
+// 执行用时：0 ms, 在所有 Go 提交中击败了100.00%的用户
+// 内存消耗：2.2 MB, 在所有 Go 提交中击败了85.14%的用户
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	prefix := strs[0]
+	count := len(strs)
+	for i := 1; i < count; i++ {
+		prefix = lcp(prefix, strs[i])
+		if len(prefix) == 0 {
+			break
+		}
+	}
+	return prefix
+}
+
+func lcp(str1, str2 string) string {
+	length := min(len(str1), len(str2))
+	index := 0
+	for index < length && str1[index] == str2[index] {
+		index++
+	}
+	return str1[:index]
+}
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+// 方法二：纵向扫描
+
+// 执行用时：0 ms, 在所有 Go 提交中击败了100.00%的用户
+// 内存消耗：2.2 MB, 在所有 Go 提交中击败了96.62%的用户
+func longestCommonPrefix(strs []string) string {
+	if len(strs) == 0 {
+		return ""
+	}
+	for i := 0; i < len(strs[0]); i++ {
+		for j := 1; j < len(strs); j++ {
+			if i == len(strs[j]) || strs[j][i] != strs[0][i] {
+				return strs[0][:i]
+			}
+		}
+	}
+	return strs[0]
+}
