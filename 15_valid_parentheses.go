62715fb921c50accd6eacf4289a3026c663be165
jinhua.wang <jinhua.wang@transwarp.io>
2022-03-05 15:11:08 +0800

    20220305 : 15_valid_parentheses

---
+++ 15_valid_parentheses.go
@@ -0,0 +1,33 @@
+// 15. 有效的括号
+// 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串 s ，判断字符串是否有效。
+// 有效字符串需满足：
+// 左括号必须用相同类型的右括号闭合。
+// 左括号必须以正确的顺序闭合。
+// https://leetcode-cn.com/problems/valid-parentheses/
+
+// 执行用时：0 ms, 在所有 Go 提交中击败了100.00%的用户
+// 内存消耗：2.1 MB, 在所有 Go 提交中击败了82.80%的用户
+
+func isValid(s string) bool {
+	n := len(s)
+	if n%2 == 1 {
+		return false
+	}
+	pairs := map[byte]byte{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+	stack := []byte{}
+	for i := 0; i < n; i++ {
+		if pairs[s[i]] > 0 {
+			if len(stack) == 0 || stack[len(stack)-1] != pairs[s[i]] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+		} else {
+			stack = append(stack, s[i])
+		}
+	}
+	return len(stack) == 0
+}
