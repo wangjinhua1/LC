9adef32aacb769ed9e4ad8db56796540241746be
jinhua.wang <jinhua.wang@transwarp.io>
2022-03-05 14:56:19 +0800

    20220305 : 14_delete_node_of_list

---
+++ 14_delete_node_of_list.go
@@ -0,0 +1,18 @@
+// 14. Delete the penultimate node of the linked list
+// 给你一个链表，删除链表的倒数第 n 个结点，并且返回链表的头结点。
+
+// 双指针
+// 执行用时：0 ms, 在所有 Go 提交中击败了100.00%的用户
+// 内存消耗：2.1 MB, 在所有 Go 提交中击败了82.80%的用户
+func removeNthFromEnd(head *ListNode, n int) *ListNode {
+	dummy := &ListNode{0, head}
+	first, second := head, dummy
+	for i := 0; i < n; i++ {
+		first = first.Next
+	}
+	for ; first != nil; first = first.Next {
+		second = second.Next
+	}
+	second.Next = second.Next.Next
+	return dummy.Next
+}
