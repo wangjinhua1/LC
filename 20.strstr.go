1eb0e4b29fea0b3e4c2b378c223e1d613f724426
jinhua.wang <jinhua.wang@transwarp.io>
2022-03-30 19:24:02 +0800

    20220330 : 20_strstr

---
+++ 20.strstr.go
@@ -0,0 +1,16 @@
+// 实现 strStr() 函数。
+// 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回  -1 。
+
+func strStr(haystack, needle string) int {
+	n, m := len(haystack), len(needle)
+outer:
+	for i := 0; i+m <= n; i++ {
+		for j := range needle {
+			if haystack[i+j] != needle[j] {
+				continue outer
+			}
+		}
+		return i
+	}
+	return -1
+}
