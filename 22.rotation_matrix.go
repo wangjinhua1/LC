4fdf558cd8e8271992cff95e24542e70151efcba
jinhua.wang <jinhua.wang@transwarp.io>
2022-04-01 20:16:43 +0800

    20220401 : 22.rotation_matrix

---
+++ 22.rotation_matrix.go
@@ -0,0 +1,19 @@
+// 给定一个 n × n 的二维矩阵 matrix 表示一个图像。请你将图像顺时针旋转 90 度。
+// 你必须在 原地 旋转图像，这意味着你需要直接修改输入的二维矩阵。请不要 使用另一个矩阵来旋转图像。
+// 来源：力扣（LeetCode）
+// 链接：https://leetcode-cn.com/problems/rotate-image
+
+// eg:
+//    1 2 3        7 4 1
+//    4 5 6  ===>  8 5 2
+//    7 8 9        9 6 3
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		for j := 0; j < (n+1)/2; j++ {
+			matrix[i][j], matrix[n-j-1][i], matrix[n-i-1][n-j-1], matrix[j][n-i-1] =
+				matrix[n-j-1][i], matrix[n-i-1][n-j-1], matrix[j][n-i-1], matrix[i][j]
+		}
+	}
+}
